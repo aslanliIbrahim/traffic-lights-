92f6aadd909f453f598bf25eea1893d951b529e0
aslanliIbrahim <ibrahimra@code.edu.az>
2021-07-15 18:42:52 +0400

    trafic lights

---
+++ 19.07.21/main.js
@@ -0,0 +1,104 @@
+const red = document.querySelector('#red')
+red.onclick = function(){
+
+}
+const yellow = document.querySelector('#yellow')
+yellow.onclick = function(){
+
+}
+const green = document.querySelector('#green')
+green.onclick = function(){
+
+}
+setInterval(function(){
+    red.style.backgroundColor = "#FF1E00",
+    red.style.boxShadow = "0px 0px 50px #FF1E00"
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708",
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00"
+ }, 5000)
+
+ setInterval(function(){
+    yellow.style.backgroundColor = "#FFE310",
+    yellow.style.boxShadow = "0px 0px 50px #FFFF20",
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00"
+ }, 7000)
+
+setInterval(function(){
+    green.style.backgroundColor = "#00FF1A",
+    green.style.boxShadow = "0px 0px 50px #00FF1A",
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708"
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+},9000)
+
+setInterval(function(){
+    red.style.backgroundColor = "#FF1E00",
+    red.style.boxShadow = "0px 0px 50px #FF1E00",
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708"
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00"
+
+ }, 11000)
+
+ setInterval(function(){
+    yellow.style.backgroundColor = "#FFE310",
+    yellow.style.boxShadow = "0px 0px 50px #FFFF20",
+    green.style.backgroundColor= "#00FF00",
+    green.style.boxShadow = "0px 0px 0px #00FF00",
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+ }, 15000)
+
+ setInterval(function(){
+    green.style.backgroundColor = "#00FF1A",
+    green.style.boxShadow = "0px 0px 50px #00FF1A",
+    red.style.backgroundColor = "#B20000"
+    red.style.boxShadow = "0px 0px 0px #B20000"
+    yellow.style.backgroundColor = "#ADB708",
+    yellow.style.boxShadow = "0px 0px 0px #ADB708"
+ }, 17000)
+
+
+
+// function changecolor(){
+//     var red = document.querySelector("#red")
+//     red.onclick = function(){}
+//     var yellow = document.querySelector("#yellow")
+//     yellow.onclick = function(){}
+//     var green = document.querySelector("#green")
+//     green.onclick = function(){}
+
+
+//     setTimeout(function(){
+//         red.style.backgroundColor = "#FF1E00",
+//         red.style.boxShadow = "0px 0px 0px #FF1E00"
+//     }, 5000)
+//     setTimeout(function(){
+//         yellow.style.backgroundColor = "#FFE310",
+//         yellow.style.boxShadow = "0px 0px 0px #FFFF20 "
+//     },7000)
+
+//     setTimeout(function(){
+//         green.style.backgroundColor = "#00FF1A",
+//         green.style.boxShadow = "0px 0px 0px #00FF1A"
+//     },9000)
+
+//     setTimeout(changecolor(), 9005);
+// }
+
+
+ 
+
+
+
+
+
+ 
+
